d9209d4101c693df4d8adac14c42bd8aae9b173a
jkociniak <jan.kociniak@gmail.com>
2024-09-05 20:41:30 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='irma',
+    version='0.1',
+    packages=['src', 'src.utils', 'src.models', 'src.datasets', 'src.manifolds', 'conf'],
+    url='',
+    license='',
+    author='Jan Kociniak',
+    author_email='jan.kociniak@student.uva.nl',
+    description='Implicit Riemannian Manifold Approximation'
+)
